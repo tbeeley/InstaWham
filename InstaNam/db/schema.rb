d0dbcfa951495c4475629300fa9934803edcfadc
Thomas Beeley <tbeeley@gmail.com>
2014-08-19 18:27:30 +0100

    AWS setup

--- InstaNam/db/schema.rb
+++ InstaNam/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818125002) do
+ActiveRecord::Schema.define(version: 20140819102519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20140818125002) do
     t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
 end
