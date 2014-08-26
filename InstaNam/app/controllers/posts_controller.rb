f16fa0e6f766545cf6577ef81ec871c9377917bd
Thomas Beeley <tbeeley@gmail.com>
2014-08-26 16:19:45 +0100

    price added

--- InstaNam/app/controllers/posts_controller.rb
+++ InstaNam/app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post].permit(:title, :picture, :tag_list, :charges))
+    @post = Post.new(params[:post].permit(:title, :picture, :tag_list, :price))
     @post.save
     redirect_to '/posts'
   end
--- InstaNam/db/schema.rb
+++ InstaNam/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820125135) do
+ActiveRecord::Schema.define(version: 20140826151226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140820125135) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.integer  "price"
   end
 
   create_table "posts_tags", id: false, force: true do |t|
