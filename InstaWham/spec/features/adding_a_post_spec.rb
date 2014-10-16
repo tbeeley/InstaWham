ce3320fa969c46ad2cc7060335034dea854ef3d1
Thomas Beeley <tbeeley@gmail.com>
2014-10-16 18:55:29 +0100

    README changed

---
+++ InstaWham/spec/features/adding_a_post_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+describe 'posts' do
+  context 'no posts' do
+
+    it 'should let the user know there are no posts' do
+      visit '/posts'
+      expect(page).to have_content('There are currently no posts')
+    end
+
+  end
+
+  context 'with posts' do
+
+    before do
+      Post.create(title: "Charlie don't surf")
+    end
+
+    it 'should list the posts' do
+      visit '/posts'
+      expect(page).to have_content("Charlie don't surf")
+    end
+
+    # it 'should list the price of each post' do
+    #   visit '/posts'
+    #   expect(page).to have_content"10")
+    # end
+
+  end
+
+  describe 'creating posts' do
+
+    it 'adds the posts to a form' do
+      visit '/posts'
+      click_link('Add new post')
+      fill_in 'post_title', with: 'A brand new post'
+      click_button('Create post')
+
+      expect(page).to have_content('A brand new post')
+      expect(current_path).to eq '/posts'
+    end
+
+    it 'displays no image if no picture attached' do
+      visit '/posts'
+      click_link('Add new post')
+      fill_in 'post_title', with: 'A brand new post'
+      click_button('Create post')
+
+      expect(page).not_to have_css 'img.uploaded-pic'
+    end
+
+    it 'can attach an image to a post' do
+      visit('/posts')
+      click_link('Add new post')
+      fill_in 'post_title', with: 'A brand new post'
+      attach_file 'Picture', Rails.root.join('spec/images/children.png')
+      click_button 'Create post'
+
+      expect(page).to have_content('A brand new post')
+      expect(page).to have_css 'img.uploaded-pic'
+    end
+
+    it 'can attach a price to a post' do
+      visit '/posts'
+      click_link('Add new post')
+      fill_in 'post_price', with: '100'
+      click_button 'Create post'
+
+      expect(page).to have_content('$100')
+    end
+
+
+  end
+
+end
---
+++ InstaWham/spec/features/tagging_feature_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'tagging posts' do
+	it 'displays tags as links under posts' do
+		visit '/posts'
+      click_link('Add new post')
+      fill_in 'post_title', with: 'A brand new post'
+      fill_in 'Tags', with: '#WW1, #Paris'
+      click_button('Create post')
+
+      expect(page).to have_link '#WW1'
+      expect(page).to have_link '#Paris'
+	end
+end
+
+describe 'filtering by tags' do
+
+	before do 
+		Post.create(title: 'Children in Nam', tag_list: '#Nam, #Youth')
+		Post.create(title: 'Children in Nam B', tag_list: '#Nam, #Littleuns')
+	end
+
+		it 'filters to show only tagged posts' do
+			visit '/posts'
+			click_link '#Youth'
+
+			expect(page).to have_css 'h1', text: 'Posts tagged with #Youth'
+			expect(page).not_to have_content 'Children in Nam B'
+			expect(page).to have_content 'Children in Nam'
+			
+		end
+
+		it 'accessible via pretty URLs' do
+			visit '/tags/Youth'
+
+			expect(page).to have_content 'Children in Nam'
+		end
+
+end
